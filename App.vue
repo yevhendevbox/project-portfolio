85234769dc049748409e6bff180bbd116cc0a2d7
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-05-16 20:55:21 +0300

    feat: dynamic works pages initial commit

--- App.vue
+++ App.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="main">
-    <the-navbar />
-    <div class="page-wrapper">
-      <NuxtPage />
-    </div>
-    <the-footer />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/strapi',
-    '@nuxtjs/eslint-module',
-    '@nuxt/devtools-edge',
-    'nuxt-icon',
-  ],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/devtools-edge', 'nuxt-icon'],
   app: {
     pageTransition: {
       name: 'page',
       mode: 'out-in',
     },
   },
-  runtimeConfig: {
-    public: {
-      url: process.env.NUXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
-      prefix: '/api',
-      version: 'v4',
-      cookie: {},
-      cookieName: 'strapi_jwt',
-    },
-  },
+  // runtimeConfig: {
+  //   public: {
+  //     url: process.env.NUXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
+  //     prefix: '/api',
+  //     version: 'v4',
+  //     cookie: {},
+  //     cookieName: 'strapi_jwt',
+  //   },
+  // },
   css: ['@/assets/css/styles.css'],
 });
