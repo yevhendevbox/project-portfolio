2ce821db20a07d8e06844c34df78237232f94a05
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-05-26 13:41:22 +0300

    feat: mobile styling finished

--- pages/work/[id].vue
+++ pages/work/[id].vue
@@ -17,46 +17,45 @@
           </div>
         </div>
         <div class="project-description__text">
-          <div>
-            <strong>Home</strong> - is an all-in-one social service that will
-            cover all aspects of your communication with your home and
-            neighbors.
-            <ul>
-              <li>Internal notification and news system</li>
-              <li>
-                Private messages, chat with selected residents and general OSBB
-                group with all residents or OSBB's modules separately
-              </li>
-              <li>
-                Residents independently form a budget, determine contributions
-                for the maintenance of the house, the sequence of solving
-                problems. This makes it possible to quickly respond to
-                emergencies, to decide what needs to be done in the house or on
-                the adjacent territory in the first place, to ensure the
-                protection of the personal and common property of residents
-              </li>
-              <li>
-                The ability to choose a service provider such as (water,
-                electricity, gas), and pay utility bills
-              </li>
-              <li>
-                The best offers and wishes can be implemented in the OSBB by
-                residents through internal voting system
-              </li>
-              <li>
-                Transparency of expense. The residents' funds go exclusively to
-                the needs of their home and are spent rationally. The head of
-                the OSBB reports directly to the residents, so they are always
-                aware of what the funds were spent on
-              </li>
-            </ul>
-            <div class="project-description--tech">
-              <h5>Tech stack:</h5>
-              <p>
-                Vue.js(v3), Vuex, VueRouter, Axios, TypeScript, PrimeVue,
-                PrimeIcons, SCSS
-              </p>
-            </div>
+          <span>
+            Home - is an all-in-one social service that will cover all aspects
+            of your communication with your home and neighbors.
+          </span>
+          <ul>
+            <li>Internal notification and news system</li>
+            <li>
+              Private messages, chat with selected residents and general OSBB
+              group with all residents or OSBB's modules separately
+            </li>
+            <li>
+              Residents independently form a budget, determine contributions for
+              the maintenance of the house, the sequence of solving problems.
+              This makes it possible to quickly respond to emergencies, to
+              decide what needs to be done in the house or on the adjacent
+              territory in the first place, to ensure the protection of the
+              personal and common property of residents
+            </li>
+            <li>
+              The ability to choose a service provider such as (water,
+              electricity, gas), and pay utility bills
+            </li>
+            <li>
+              The best offers and wishes can be implemented in the OSBB by
+              residents through internal voting system
+            </li>
+            <li>
+              Transparency of expense. The residents' funds go exclusively to
+              the needs of their home and are spent rationally. The head of the
+              OSBB reports directly to the residents, so they are always aware
+              of what the funds were spent on
+            </li>
+          </ul>
+          <div class="project-description--tech">
+            <h5>Tech stack:</h5>
+            <p>
+              Vue.js(v3), Vuex, VueRouter, Axios, TypeScript, PrimeVue,
+              PrimeIcons, SCSS
+            </p>
           </div>
           <div>I am from: {{ route.params.id }}</div>
         </div>
