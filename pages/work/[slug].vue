b54fb93439fe696618ea00038ff7492f5bf47d41
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-06-21 21:37:10 +0300

    ref: dynamic page fix

--- pages/work/[slug].vue
+++ pages/work/[slug].vue
@@ -80,7 +80,7 @@ const project = ref({
 
 const route = useRoute();
 const slug = route.params.slug;
-const query = groq`*[_type == 'project']{'id': _id, title, 'slug': slug.current, excerpt,  description, 'posterUri': poster.asset._ref, projectGithubUrl, productionUrl, techStack}`;
+const query = groq`*[_type == 'project']{'id': _id, 'title': title.en, 'slug': slug.current, 'excerpt': excerpt.en,  description, 'posterUri': poster.asset._ref, projectGithubUrl, productionUrl, techStack}`;
 const sanity = useSanity();
 const { data } = await useAsyncData(() => sanity.fetch(query));
 
